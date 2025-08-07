7aba09140c9f1243eb5b707337a86b6eddc806ec
Lamont <missionpossible23@protonmail.com>
2025-08-06 20:38:26 -0400

    Update PoolConnect.jsx

--- Components/PoolConnect/PoolConnect.jsx
+++ Components/PoolConnect/PoolConnect.jsx
@@ -16,18 +16,18 @@ const PoolConnect = () => {
 
         <div className={Style.PoolConnect_box_Middle}>
           <Image src={images.wallet} alt="wallet" height={80} width={80} />
-          <p>Your active Jackrabbit liquidity positions will appear here.</p>
+          <p>Confidential Liquidiyt positions.</p>
           <button>Connect Wallet</button>
         </div>
 
         <div className={Style.PoolConnect_box_info}>
           <div className={Style.PoolConnect_box_info_left}>
             <h5>Learn about providing liquidity</h5>
-            <p>Check out our Jackrabbit LP walkthrough and migrate guide</p>
+            <p>Check out our encrypted positions</p>
           </div>
           <div className={Style.PoolConnect_box_info_right}>
             <h5>Top pools</h5>
-            <p>Explore Jackrabbit Analytics</p>
+            <p>Explore Coffhee Analytics</p>
           </div>
         </div>
       </div>
